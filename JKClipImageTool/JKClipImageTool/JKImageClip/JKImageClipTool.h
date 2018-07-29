c181e9f2038054f7ec54ebf7a00437c4e1d5faa1
albertCoder <jkdev123cool@gmail.com>
2018-07-29 12:53:59 +0800

    现在可以按照宽高比裁剪了

--- JKClipImageTool/JKClipImageTool/JKImageClip/JKImageClipTool.h
+++ JKClipImageTool/JKClipImageTool/JKImageClip/JKImageClipTool.h
@@ -15,12 +15,26 @@ typedef enum : NSUInteger {
     JKImageClipTypeSquare = 1, // 裁剪方形图片
     JKImageClipTypeCircle = 2, // 裁剪圆形图片
     JKImageClipTypeFree = 3,   // 自由裁剪图片
+    
+    /** 是否带导航条是为了控制上边距 */
     JKImageClipTypeJustShowImageWithNavBar = 4, // 仅仅展示图片，带导航条
+    
+    /** 是否带导航条是为了控制上边距 */
     JKImageClipTypeFreeWithNavBar = 5, // 自由裁剪图片，带导航条
     
 } JKImageClipType;
 
 @interface JKImageClipTool : NSObject
+
+// ========================================Attention========================================
+
+// 当你需要把视图放在一个控制器的view中，并带有导航条时，需要的操作请查阅<JKImageClipActionProtocol>协议
+// 具体可参照ClipCircleViewController的写法
+
+// important!! 暂不支持横屏操作，请保证使用时处于竖屏状态！！！
+
+// ========================================Attention========================================
+
 /**
  * 裁剪图片
  * image : 要裁剪的图片
@@ -33,8 +47,9 @@ typedef enum : NSUInteger {
  * return value: 返回的对象是对应的view，使用id+协议方便操作，如不放心内存问题，可在不用的时候将返回值置为nil
  */
 + (id<JKImageClipActionProtocol>)showWithImage:(UIImage *)image superView:(UIView *)superView imageClipType:(JKImageClipType)imageClipType autoSavaToAlbum:(BOOL)autoSavaToAlbum complete:(void(^)(UIImage *image))complete cancel:(void(^)(void))cancel;
+
 /**
- * 裁剪图片
+ * 按比例裁剪图片
  * image : 要裁剪的图片
  * superView : 父视图，注意frame一定要是屏幕大小，否则可能会出现问题
  *             传入父试图，则自己处理父视图显示动画，如果传nil，则默认添加到keywindow
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # JKImageClipTool
-简单的图片裁剪工具，支持正方形裁剪和自由裁剪，还可以用于预览展示图片。
+简单的图片裁剪工具，支持正方形、圆形、宽高比裁剪和自由裁剪，还可以用于预览展示图片。
+
+
+
+plain带textField样式  
+
+<img src="https://raw.github.com/Jacky-An/JKAlertX/master/Example/Snapshots/plain.PNG" width="375"><br/>
+
+actionSheet样式  
+
+<img src="https://raw.github.com/Jacky-An/JKAlertX/master/Example/Snapshots/actionSheet.PNG" width="375"><br/>
+
+collectionSheet样式  
+
+<img src="https://raw.github.com/Jacky-An/JKAlertX/master/Example/Snapshots/collectionSheet.PNG" width="375"><br/>
+
+HUD样式  
+
+<img src="https://raw.github.com/Jacky-An/JKAlertX/master/Example/Snapshots/HUD.PNG" width="375"><br/>
